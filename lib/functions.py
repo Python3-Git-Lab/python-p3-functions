671ac1e564cfbd12a392d1964725a55fa63d0921
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-05 13:24:26 +0300

    Completed assignment

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default();
 
 def add(num1, num2):
-    pass
+    return num1+num2
+sum= add(1,2)
+print(sum)
 
 def halve(number):
-    pass
+    if not isinstance(number, (int, float)):
+        return None
+    return number/2
+half= halve(4)
+print(half)
